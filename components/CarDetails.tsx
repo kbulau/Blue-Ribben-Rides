46914ec8497231a472fed99e08e98b798425c72d
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-12-13 14:44:58 -0600

    add different car angles to modal

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Image from 'next/image';
 import {Fragment} from 'react';
 import {Dialog, Transition} from '@headlessui/react';
+import {generateCarImageUrl} from '@/utils';
 interface CarDetailsProps {
   isOpen: boolean;
   closeModal: () => void;
@@ -51,7 +52,7 @@ const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
                   <div className="flex-1 flex flex-col gap-3">
                     <div className="relative w-full h-40 bg-pattern bg-cover bg-center rouded-lg">
                       <Image
-                        src="/hero.png"
+                        src={generateCarImageUrl(car)}
                         alt="car model"
                         fill
                         priority
@@ -61,7 +62,7 @@ const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
                     <div className="flex gap-3">
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, 29)}
                           alt="car model"
                           fill
                           priority
@@ -70,7 +71,7 @@ const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
                       </div>
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, 33)}
                           alt="car model"
                           fill
                           priority
@@ -79,7 +80,7 @@ const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
                       </div>
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, 13)}
                           alt="car model"
                           fill
                           priority

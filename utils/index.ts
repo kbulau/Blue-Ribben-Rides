5db0b863f2ee94a08a150c33e1efee7c4f71dd78
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-12-13 15:21:21 -0600

    edit API to take custom queries and get correct vehicle images

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,14 @@
-import {CarProps} from '@/types';
+import {CarProps, FilterProps} from '@/types';
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const {manufacturer, year, model, limit, fuel} = filters;
   const headers = {
     'X-RapidAPI-Key': 'c0bc50847emshf9e876cbb63bfd0p10f6c1jsn088835a564bf',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   };
 
   const response = await fetch(
-    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=r8',
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     }

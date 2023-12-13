3648f4c6a2a0a75b64f9612e4f7988ae5a5e64e9
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-12-13 14:35:20 -0600

    add API for car images in utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import {CarProps} from '@/types';
+
 export async function fetchCars() {
   const headers = {
     'X-RapidAPI-Key': 'c0bc50847emshf9e876cbb63bfd0p10f6c1jsn088835a564bf',
@@ -27,4 +29,16 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCardImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage');
+  const {make, year, model} = car;
+  url.searchParams.append('customer', 'hrjavascript-mastery');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angle', `${angle}`);
+  return `${url}`;
 };

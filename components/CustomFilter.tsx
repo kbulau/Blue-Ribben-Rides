552dccc91ddabc1b7cbfaebb12b78e4ef57c4c51
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-12-13 15:58:43 -0600

    finish filter functionality'

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -5,11 +5,22 @@ import {useRouter} from 'next/navigation';
 import {Listbox, Transition} from '@headlessui/react';
 import {useState, Fragment} from 'react';
 import {CustomFilterProps} from '@/types';
+import {updateSearchParams} from '@/utils';
 const CustomFilter = ({title, options}: CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0]);
+  const router = useRouter();
+  const handleUpdateParams = (e: {title: string; value: string}) => {
+    const newPathName = updateSearchParams(title, e.value.toLocaleLowerCase());
+    router.push(newPathName);
+  };
   return (
     <div className="w-fit">
-      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+          handleUpdateParams(e);
+        }}>
         <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">
             <span className="block truncate">{selected.title}</span>
@@ -36,7 +47,14 @@ const CustomFilter = ({title, options}: CustomFilterProps) => {
                       active ? 'bg-primary-blue text-white' : 'text-gray-900'
                     }`
                   }>
-                  {({selected}) => <span>{option.title}</span>}
+                  {({selected}) => (
+                    <span
+                      className={`block truncate ${
+                        selected ? 'font-medium' : 'font-normal'
+                      }`}>
+                      {option.title}
+                    </span>
+                  )}
                 </Listbox.Option>
               ))}
             </Listbox.Options>

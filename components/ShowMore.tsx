a45f8ccefde2b5614a4a701af8441a412e90663a
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-12-13 18:11:46 -0600

    add show more button

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,7 +1,23 @@
+'use client';
 import React from 'react';
-
-const ShowMore = () => {
-  return <div>ShowMore</div>;
+import {useRouter} from 'next/navigation';
+import {ShowMoreProps} from '@/types';
+import {CustomButton} from '.';
+const ShowMore = ({pageNumber, isNext}: ShowMoreProps) => {
+  const router = useRouter();
+  const handleNavigation = () => {};
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ShowMore;
